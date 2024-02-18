d174c1a0b1e261ac60dc98562bb34b6348bb6221
lhl <lhl@randomfoo.net>
2024-02-18 23:07:27 +0900

    nvidia results

--- show-mem.py
+++ show-mem.py
@@ -1,5 +1,7 @@
 # 7900 XTX
 bytes = 5578280960
 
+# Q-Lora
+# bytes = 4101840896
 mebibytes = bytes / 2**20
 print(mebibytes)
---
+++ train-4090-qlora.sh
@@ -0,0 +1,20 @@
+time CUDA_VISIBLE_DEVICES=0 python src/train_bash.py \
+    --stage pt \
+    --do_train \
+    --model_name_or_path TinyLlama/TinyLlama-1.1B-Chat-v1.0 \
+    --dataset wiki_demo \
+    --finetuning_type lora \
+    --lora_target q_proj,v_proj \
+    --output_dir test \
+    --overwrite_cache \
+    --per_device_train_batch_size 4 \
+    --gradient_accumulation_steps 4 \
+    --lr_scheduler_type cosine \
+    --logging_steps 10 \
+    --save_steps 1000 \
+    --learning_rate 5e-5 \
+    --num_train_epochs 3.0 \
+    --plot_loss \
+    --fp16 \
+    --quantization_bit 4 \
+    --overwrite_output_dir
